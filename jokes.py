2e6575d6f96f866423ed10eaff36747e2370050b
Aman Sharma <aman889678@gmail.com>
2024-10-26 17:07:09 +0530

    Update jokes.py

--- jokes.py
+++ jokes.py
@@ -1,4 +1,4 @@
 # pip3 install pyjokes run this cmnd in powershell to install pyjokes so that you can import module
 import pyjokes
 print(pyjokes.get_joke()) #for more info go to https://pypi.org/project/pyjokes/ 
-hi
+hi hello jiii
