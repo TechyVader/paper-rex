6d2a258764a08105cf8d3802815e0b4c7e887c96
Raj Chahar <pahadibarfi@gmail.com>
2024-10-26 17:03:25 +0530

    Add files via upload

---
+++ jokes.py
@@ -0,0 +1,3 @@
+# pip3 install pyjokes run this cmnd in powershell to install pyjokes so that you can import module
+import pyjokes
+print(pyjokes.get_joke()) #for more info go to https://pypi.org/project/pyjokes/ 
